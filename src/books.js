a8a72f5d062a2991637123f90abfdbf871ec86f3
Catalin Petre <petrecatalin42@gmail.com>
2021-11-04 02:40:45 +0000

    React-basics

---
+++ src/books.js
@@ -0,0 +1,20 @@
+export const data = [
+  {
+    id: 1,
+    img: 'https://m.media-amazon.com/images/I/91hIsjFuICL._AC_UL640_FMwebp_QL65_.jpg',
+    title: 'I Love You to the Moon and Back',
+    author: 'Amelia Hepworth',
+  },
+  {
+    id: 2,
+    img: 'https://m.media-amazon.com/images/I/71aLultW5EL._AC_UY436_FMwebp_QL65_.jpg',
+    title: 'Our Class is a Family',
+    author: 'Shannon Olsen',
+  },
+  {
+    id: 3,
+    img: 'https://m.media-amazon.com/images/I/71qWVhqYuOL._AC_UY436_FMwebp_QL65_.jpg',
+    title: 'The Vanishing Half: A Novel',
+    author: 'Brit Bennet',
+  },
+]
